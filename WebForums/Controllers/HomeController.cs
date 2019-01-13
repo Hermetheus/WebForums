37afcf3905097b0489a4fbebf57193506c964396
Allan Olson <allanmolson@gmail.com>
2019-01-13 12:25:32 -0600

    PostService & Home Controller

--- WebForums/Controllers/HomeController.cs
+++ WebForums/Controllers/HomeController.cs
@@ -1,18 +1,66 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebForums.Data;
+using WebForums.Data.Models;
 using WebForums.Models;
+using WebForums.Models.Forum;
+using WebForums.Models.Home;
+using WebForums.Models.Post;
 
 namespace WebForums.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IPost _postService;
+
+
+        public HomeController(IPost postService)
+        {
+            _postService = postService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var model = BuildHomeIndexModel();
+
+
+            return View(model);
+        }
+
+        private HomeIndexModel BuildHomeIndexModel()
+        {
+            var latestPosts = _postService.GetLatestPosts(10);
+
+            var posts = latestPosts.Select(post => new PostListingModel
+            {
+                Id = post.Id,
+                Title = post.Title,
+                AuthorId = post.User.Id,
+                AuthorName = post.User.UserName,
+                AuthorRating = post.User.Rating,
+                DatePosted = post.Created.ToString(),
+                RepliesCount = post.Replies.Count(),
+                Forum = GetForumListingForPost(post)
+            });
+
+            return new HomeIndexModel
+            {
+                LatestPosts = posts,
+                SearchQuery = ""
+            };
+        }
+
+        private ForumListingModel GetForumListingForPost(Post post)
+        {
+            var forum = post.Forum;
+
+            return new ForumListingModel
+            {
+                Name = forum.Title,
+                ImageUrl = forum.ImageUrl,
+                Id = forum.Id
+            };
         }
 
         public IActionResult About()
---
+++ WebForums/Models/Home/HomeIndexModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using WebForums.Models.Post;
+
+namespace WebForums.Models.Home
+{
+    public class HomeIndexModel
+    {
+        public string SearchQuery { get; set; }
+        public IEnumerable<PostListingModel> LatestPosts { get; set; }
+
+    }
+}
