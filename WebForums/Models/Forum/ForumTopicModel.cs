c707841741103e2ff87349b53c282fe2078e7e81
Allan Olson <allanmolson@gmail.com>
2019-01-15 22:02:55 -0600

    Add Styles, Format Topic

--- WebForums/Models/Forum/ForumTopicModel.cs
+++ WebForums/Models/Forum/ForumTopicModel.cs
@@ -7,5 +7,7 @@ namespace WebForums.Models.Forum
     {
         public ForumListingModel Forum { get; set; }
         public IEnumerable<PostListingModel> Posts { get; set; }
+
+        public string SearchQuery { get; set; }
     }
 }
